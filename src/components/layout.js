a43a51adc1fb4bbdb04d6729a78acbd273c535df
hymos <almaasizdihar@gmail.com>
2023-01-03 09:40:11 +0700

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ export function Container ({id, className, children, fluid}) {
 
   const isFluidWidth = fluid
     ? 'w-full'
-    : 'm-auto sm:max-w-[620px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px]'
+    : 'm-auto px-5 sm:px-0 sm:max-w-[620px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px]'
 
   return (
     <div 
@@ -19,7 +19,7 @@ export function Container ({id, className, children, fluid}) {
 export function Grid ({id, className, children}) {
 
   const defaultClassName = `w-full grid`
-  const defaultCols = 'grid-cols-4 md:grid-cols-8 lg:grid-cols-12'
+  const defaultCols = 'grid-cols-4 sm:grid-cols-8 md:grid-cols-12'
 
   return (
     <div
@@ -51,16 +51,26 @@ export function Col ({id, className, children}) {
   
   Container
   - ada 2 tipe width (fluid dan default)
+  - fluid
+    - width : 100%
+  - default
+    - base : 100%, padding-x-20px
+    - sm   : max 620
+    - md   : max 720
+    - lg   : max 960
+    - xl   : max 1140
 
   Grid 
   - default width
   - jumlah kolom 4/8/12 menyesuaikan viewport 
+    - base : 4
+    - md   : 8
+    - lg   : 12
 
   Col
   - kolom2 pada grid
 
   references: https://github.com/hymos-two/forzanove-beta/blob/main/components/grid.js
-
 
 
   tailwind viewport list (default)
