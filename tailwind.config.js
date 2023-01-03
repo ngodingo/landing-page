82638c860c63ea1c927cbebb3c2730f9b855b648
hymos <almaasizdihar@gmail.com>
2023-01-03 21:37:29 +0700

    add comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,18 +10,22 @@ module.exports = {
         'lexend-deca': ['var(--font-lexend-deca)']
       },
       fontSize: {
+        // display
         'display-1': ['64px', '150%'],
         'display-2': ['48px', '150%'],
         'display-3': ['44px', '150%'],
+        // heading
         'heading-1': ['40px', '140%'],
         'heading-2': ['36px', '140%'],
         'heading-3': ['32px', '140%'],
+        // body
         'body-1':    ['20px', '180%'],
         'body-2':    ['16px', '180%'],
         'body-3':    ['14px', '180%'],
         'body-4':    ['12px', '180%'],
       },
       colors: {
+        // ocean blue
         primary: {
           10: '#9396EC',
           30: '#5C61D6',
@@ -29,6 +33,7 @@ module.exports = {
           70: '#3D408F',
           90: '#262973',
         },
+        // independence
         secondary: {
           10: '#9BA0B1',
           30: '#70788F',
@@ -36,6 +41,7 @@ module.exports = {
           70: '#333D4D',
           90: '#1D2430',
         },
+        // pear
         accent: {
           10: '#E8E47D',
           30: '#D6D25C',
@@ -43,6 +49,7 @@ module.exports = {
           70: '#ADA81F',
           90: '#8A850F',
         },
+        // spanish grey
         neutral: {
           10: '#CCCCCC',
           30: '#B3B3B3',
