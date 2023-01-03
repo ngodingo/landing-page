c72cf7c60b3704b0e6e5bd730190ee3501cd8f66
hymos <almaasizdihar@gmail.com>
2023-01-03 11:49:33 +0700

    add typography custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lexend-deca': ['var(--font-lexend-deca)']
+      },
+      fontSize: {
+        'display-1': ['64px', '150%'],
+        'display-2': ['48px', '180%'],
+        'display-3': ['44px', '140%'],
+        'heading-1': ['40px', '140%'],
+        'heading-2': ['36px', '140%'],
+        'heading-3': ['32px', '140%'],
+        'body-1':    ['20px', '180%'],
+        'body-2':    ['16px', '180%'],
+        'body-3':    ['14px', '180%'],
+        'body-4':    ['12px', '180%'],
+      },
+    },
   },
   plugins: [],
 }
