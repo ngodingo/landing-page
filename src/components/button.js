988e64a2dce0beb7a0bf57dc298c135a3e00b0d8
hymos <almaasizdihar@gmail.com>
2023-01-04 09:15:54 +0700

    update button component

--- src/components/button.js
+++ src/components/button.js
@@ -4,28 +4,50 @@ import Link from "next/link"
 export function Button ({text, href, theme, className}) {
 
   const btnTheme = {
-    primary: 'bg-primary-50 border-2 border-primary-50 text-white hover:border-primary-90 hover:shadow-[4px_4px] hover:shadow-primary-90',
-    secondary: 'border-2 border-primary-50 text-primary-50 hover:shadow-[4px_4px] hover:shadow-primary-50'
+    primary: {
+      normal: 'bg-primary-50 border-2 border-primary-50 text-white',
+      hover: 'group-hover:border-primary-90 group-hover:shadow-[4px_4px] group-hover:shadow-primary-90',
+      active: 'group-active:bg-transparent group-active:text-primary-90 group-active:shadow-none'
+    },
+    secondary: {
+      normal: 'border-2 border-primary-50 text-primary-50',
+      hover: 'group-hover:shadow-[4px_4px] group-hover:shadow-primary-50',
+      active: 'group-active:bg-primary-50 group-active:text-white group-active:shadow-none',
+    }
   }
 
-  const defaultClassName = 'block w-content h-12 px-6 py-2.5 rounded-3xl font-lexend-deca font-body-2 font-bold box-border mt-1 ml-1 hover:m-0 hover:mb-1 hover:mr-1 duration-[50ms]'
+  const defaultClassName = {
+    common: 'block w-content h-12 px-6 py-2.5 rounded-3xl font-lexend-deca font-body-2 font-bold box-border duration-[50ms]',
+    normal: 'mt-1 ml-1',
+    hover: 'group-hover:m-0 group-hover:mb-1 group-hover:mr-1',
+    active: 'group-active:m-0 group-active:mt-1 group-active:ml-1',
+  }
+
 
   function getBtnTheme () {
+    const {primary, secondary} = btnTheme
+
     switch (theme) {
       case 'primary':
-        return btnTheme.primary
+        return `${primary.normal} ${primary.hover} ${primary.active}`
       case 'secondary':
-        return btnTheme.secondary
+        return `${secondary.normal} ${secondary.hover} ${secondary.active}`
       default:
         return;
     }
   }
 
+  function getDefaultClassName () {
+    const {common, normal, hover, active} = defaultClassName
+    return `${normal} ${common} ${hover} ${active}`
+  }
+
+  
   return (
-    <div>
+    <div className="group">
       <Link 
         href={href} 
-        className={`${defaultClassName} ${getBtnTheme()} ${className}`} >
+        className={`${getDefaultClassName()} ${getBtnTheme()} ${className}`} >
           {text}
       </Link>
     </div>
