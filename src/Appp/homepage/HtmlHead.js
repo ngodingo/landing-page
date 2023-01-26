4a8badae8a4299f60e32fff20567664e75e964ba
hymos <almaasizdihar@gmail.com>
2023-01-26 11:15:02 +0700

    reafctoring app architecture: remove unused files

--- src/Appp/homepage/HtmlHead.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head"
-
-export function HtmlHead () {
-  return (
-    <Head>
-      <meta name="description" content="Ngodingo Community - Learn Together, Make it Flexible" />
-    </Head>
-  )
-}
