be193d035519d7dab8747b99d2b6d740dbc0ae08
hymos <almaasizdihar@gmail.com>
2023-01-31 10:52:58 +0700

    tes SSG

--- src/pages/blog/[post_slug]/index.page.js
+++ src/pages/blog/[post_slug]/index.page.js
@@ -31,7 +31,7 @@ export async function getStaticProps ({params}) {
 
 export default function Posts ({post}) {
 
-  const isUnderConstruction = true
+  const isUnderConstruction = false
   if (isUnderConstruction) return <ComingSoon />
 
   return (
