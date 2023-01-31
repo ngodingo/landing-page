9edd602540bf53fa54f6426135b0c80b4a1a9d4c
hymos <almaasizdihar@gmail.com>
2023-01-31 10:55:32 +0700

    tes selesai

--- src/pages/blog/[post_slug]/index.page.js
+++ src/pages/blog/[post_slug]/index.page.js
@@ -31,7 +31,7 @@ export async function getStaticProps ({params}) {
 
 export default function Posts ({post}) {
 
-  const isUnderConstruction = false
+  const isUnderConstruction = true
   if (isUnderConstruction) return <ComingSoon />
 
   return (
